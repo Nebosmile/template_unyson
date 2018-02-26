d690107d69e8020292683984278aff1839f694d7
Pavel <akulov_pavel88@mail.ru>
2018-02-26 12:22:46 +0200

    start tamplate

---
+++ template-parts/content-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package january2018
+ */
+
+?>
+
+
+
+
+	<div class="entry-content">
+		<?php
+			the_content();
+		?>
+	</div><!-- .entry-content -->
